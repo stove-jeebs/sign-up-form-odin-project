092699e0e71fcfa8530d0c2b6e89ea5c06f3a9b2
stove-jeebs <1876595827qq@gmail.com>
2022-05-22 20:24:39 +1200

    simple form validation

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 "use strict";
 const password = document.querySelector("#password");
 const repeat = document.querySelector("#confirm");
-const submit = document.querySelector('#submit');
-const inputsContainer = document.querySelector('#inputs-container');
-submit === null || submit === void 0 ? void 0 : submit.addEventListener("click", (event) => {
+const submit = document.querySelector("#submit");
+const inputsContainer = document.querySelector("#inputs-container");
+const inputs = document.querySelectorAll("input");
+submit.addEventListener("click", (event) => {
     event.preventDefault();
-    if (password.value != repeat.value) {
+    if (password.value != repeat.value ||
+        password.value.length == 0 ||
+        repeat.value.length == 0) {
         password.style.borderColor = "red";
         inputsContainer.classList.add("password-not-match");
         repeat.style.borderColor = "red";
--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 const password = document.querySelector("#password") as HTMLInputElement;
 const repeat = document.querySelector("#confirm") as HTMLInputElement;
-const submit = document.querySelector('#submit') as HTMLButtonElement
-const inputsContainer = document.querySelector('#inputs-container') as HTMLDivElement
+const submit = document.querySelector("#submit") as HTMLButtonElement;
+const inputsContainer = document.querySelector(
+  "#inputs-container"
+) as HTMLDivElement;
 
-submit?.addEventListener("click", (event) => {
-	event.preventDefault()
-  if (password!.value != repeat!.value) {
+const inputs = document.querySelectorAll(
+  "input"
+) as NodeListOf<HTMLInputElement>;
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (
+    password.value != repeat.value ||
+    password.value.length == 0 ||
+    repeat!.value.length == 0
+  ) {
     password.style.borderColor = "red";
     inputsContainer.classList.add("password-not-match");
     repeat.style.borderColor = "red";
